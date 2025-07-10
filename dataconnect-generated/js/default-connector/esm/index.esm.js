da5439f5546103abc8cf3dd245a84ed768e37bc7
JiHye0214 <qkrwlgp1526@gmail.com>
2025-07-10 16:51:02 -0700

    first commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'final-project',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'final-project',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
